2511f1080e691451896e68cd8f21915fb0193d18
rfeng0 <notrafaelfeng@gmail.com>
2026-07-27 16:47:43 -0700

    Added find_best_gap function

--- src/ftg_node/include/ftg_node.hpp
+++ src/ftg_node/include/ftg_node.hpp
@@ -2,6 +2,8 @@
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
+#include <vector>
+#include <cmath>
 
 /// @brief The ROS node responsible for controlling the car with Follow The Gap
 class FollowTheGapNode : public rclcpp::Node
@@ -13,7 +15,16 @@ private:
     rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr drive_msg_publisher;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_subscriber;
 
+    float gap_threshold_;
+    int gap_min_width_;
+
     /// @brief Function to be called when the lidar completes a scan
     /// @param scan_msg The scan data from the lidar
     void lidar_callback(const sensor_msgs::msg::LaserScan::ConstSharedPtr scan_msg);
+
+
+    /// @brief Function that returns the best gap found
+    /// @param ranges List of ranges from lidar
+    /// @return 2 element vector containing the start and end indexes
+    std::vector<int> find_best_gap(const std::vector<float>& ranges);
 };
--- src/ftg_node/src/ftg_node.cpp
+++ src/ftg_node/src/ftg_node.cpp
@@ -15,6 +15,51 @@ void FollowTheGapNode::lidar_callback(const sensor_msgs::msg::LaserScan::ConstSh
 {
 }
 
+std::vector<int> FollowTheGapNode::find_best_gap(const std::vector<float>& ranges)
+{
+    std::vector<int> best_gap = {0,0};
+    float best_total = 0;
+
+    int gap_start = 0;
+    float gap_total = 0;
+
+    for (size_t _i = 0; _i < ranges.size(); _i++) {
+
+        if (ranges[_i] < gap_threshold_) {
+
+            if (gap_total == 0) { // previous range was also less than threshold
+                
+                continue;
+
+            } else if (gap_total > best_total && (_i - gap_start - 1) > gap_min_width_) {
+
+                best_gap[0] = gap_start;
+                best_gap[1] = (_i - 1);
+                best_total = gap_total;
+
+            }
+
+            gap_total = 0;
+
+        } else {
+
+            if (gap_total == 0) gap_start = _i; // start of gap
+            gap_total += ranges[_i];
+
+        }
+    }
+
+    // in case the loop runs through without ending the gap
+    if (gap_total > best_total && (_i - gap_start - 1) > gap_min_width_) {
+        best_gap[0] = gap_start;
+        best_gap[1] = (ranges.size() - 1);
+        best_total = gap_total;
+    }
+
+    return best_gap;
+
+}
+
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
